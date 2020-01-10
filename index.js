c14889727688e4770def32f20c6024352e68138f
chrisp-dev <chris.paradeise@gmail.com>
2020-01-10 09:56:54 -0800

    Updated gitignore

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const express = require('express');
+const Note = require('./lib/note');
+const Db = require('./lib/db');
+const notesDB = new Db();
 const PORT = process.env.PORT || 3000;
 const app = express();
 
@@ -7,12 +10,23 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.send("What up?");
-});
-
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get('/api/notes', (req, res) => {
+    const notes = notesDB.getData();
+
+});
+
+app.post('/api/notes', (req, res) => {
+    const newNote = new Note(req.body);
+    notesDB.saveData(newNote);
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.listen(PORT, () => {
